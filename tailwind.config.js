e7ca7ef29b14801bb146d01d7e57730aae119870
axseinga <urbanowicz.uk@gmail.com>
2025-01-11 12:07:33 +0000

    add absolute paths to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,13 @@ export default {
         lightGrey3: "#F4F4F4",
         white: "#FFFFFF",
       },
+      fontSize: {
+        "body-s": ["0.875rem", "1.063rem"],
+        "body-m": ["1.125rem", "1.5rem"],
+        "heading-s": ["1.25rem", "1.5rem"],
+        "heading-m": ["1.5rem", "1.813rem"],
+        "heading-l": ["4rem", "4.875rem"],
+      },
       fontFamily: {
         inter: "Inter, sans-serif",
         inconsolata: "Inconsolata, mono",
